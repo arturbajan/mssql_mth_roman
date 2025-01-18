e860ed2bdf09889443dea5063aa4bf6ce3f808dd
Artur Bajan <artur.bajan@gmail.com>
2025-01-18 17:27:03 +0100

    add use database.

---
+++ fx_mth_roma.sql
@@ -0,0 +1,39 @@
+use dwh_staging;
+CREATE OR ALTER FUNCTION etl.mth_roman(@start date,@stop date)
+RETURNS nvarchar(max)
+BEGIN
+declare @number int;
+declare @string nvarchar(max);
+
+set @number=(select DATEDIFF(month,@start,@stop));
+
+set @string=(select STRING_AGG(q.mth_roman,',') from 
+(select w.dt_rep 
+,concat('''',case month(w.dt_rep)
+ when 1 then 'I'  
+ when 2 then'II'
+ when 3 then'III'
+ when 4 then'IV'
+ when 5 then'V'
+ when 6 then'VI'
+ when 7 then'VII'
+ when 8 then'VIII'
+ when 9 then'IX'
+ when 10 then'X'
+ when 11 then'XI'
+ when 12 then'XII'
+ else null end,' ',year(w.dt_rep),'''') mth_roman
+ from 
+(SELECT  number,
+ EOMONTH(dateadd(month,number,@start)) dt_rep
+FROM master..spt_values
+WHERE 
+    type = 'P'
+    AND 
+    number <= @number
+    )w)q);
+ RETURN @string
+END
+GO
+
+
